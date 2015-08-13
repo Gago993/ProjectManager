4559d8a4932f9a9e42ac679b81a526b341e1f543
AleksandarMitrevski <AleksandarMitrevski@users.noreply.github.com>
2015-08-13 14:32:09 +0200

    [server] authentication corrections

--- src/main/java/mk/finki/webprogramiranje/projectmanager/controller/LoginController.java
+++ src/main/java/mk/finki/webprogramiranje/projectmanager/controller/LoginController.java
@@ -3,6 +3,8 @@ package mk.finki.webprogramiranje.projectmanager.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,36 +16,35 @@ import mk.finki.webprogramiranje.projectmanager.service.MemberService;
 
 @Controller
 public class LoginController {
-
 	@Autowired
 	private MemberService service;
 	
 	@RequestMapping(value="/login", method=RequestMethod.POST, headers="content-type=application/x-www-form-urlencoded")
 	@ResponseBody
-	public String doLogin(HttpSession session, @RequestParam(name="email", required=true) String email, @RequestParam(name="password", required=true) String password) {
+	public ResponseEntity<String> doLogin(HttpSession session, @RequestParam(name="email", required=true) String email, @RequestParam(name="password", required=true) String password) {
 		//assuming 'password' is password md5 hash (not using https)
 		
 		if(LoginController.isValidEmailAddress(email) && password.length() == 32){
 			Member member = service.findByEmail(email);
 			if(member != null && member.getPassword().equals(password)){
 				LoginController.setUpSession(session, member);
-				return "ok";
+				return new ResponseEntity<String>("ok", HttpStatus.OK);
 			}else{
-				return "invalid-credentials";
+				return new ResponseEntity<String>("invalid-credentials", HttpStatus.UNAUTHORIZED);
 			}
 		}else{
-			return "invalid-credentials";
+			return new ResponseEntity<String>("invalid-credentials", HttpStatus.UNAUTHORIZED);
 		}
 	}
 	
 	@RequestMapping(value="/logout", method=RequestMethod.GET)
 	@ResponseBody
-	public String doLogout(HttpSession session){
+	public ResponseEntity<String> doLogout(HttpSession session){
 		if(session.getAttribute("id") != null){
 			session.invalidate();
-			return "ok";
+			return new ResponseEntity<String>("ok", HttpStatus.OK);
 		}else{
-			return "not-logged-in";
+			return new ResponseEntity<String>("not-logged-in", HttpStatus.UNAUTHORIZED);
 		}
 	}
 	
--- src/main/java/mk/finki/webprogramiranje/projectmanager/controller/RegisterController.java
+++ src/main/java/mk/finki/webprogramiranje/projectmanager/controller/RegisterController.java
@@ -6,29 +6,33 @@ import mk.finki.webprogramiranje.projectmanager.model.Member;
 import mk.finki.webprogramiranje.projectmanager.service.MemberService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class RegisterController {
-	
 	@Autowired
 	private MemberService service;
 	
 	@RequestMapping(value="/register", method=RequestMethod.POST, headers="content-type=application/x-www-form-urlencoded")
 	@ResponseBody
-	public String doRegister(HttpSession session, @RequestParam("email") String email, @RequestParam("password") String password, @RequestParam("firstname") String firstname, @RequestParam("lastname") String lastname) {
+	public ResponseEntity<String> doRegister(HttpSession session, @RequestParam("email") String email, @RequestParam("password") String password, @RequestParam("firstname") String firstname, @RequestParam("lastname") String lastname) {
 		//assuming 'password' is password hash (not using https)
 		
 		if(LoginController.isValidEmailAddress(email) && password.length() == 32 && !firstname.isEmpty() && !lastname.isEmpty()){
-			LoginController.setUpSession(session, service.save(new Member(email, password, firstname, lastname)));
-			return "ok";
+			if(service.findByEmail(email) == null){
+				LoginController.setUpSession(session, service.save(new Member(email, password, firstname, lastname)));
+				return new ResponseEntity<String>("ok", HttpStatus.OK);
+			}else{
+				return new ResponseEntity<String>("already-registered", HttpStatus.CONFLICT);
+			}
 		}else{
-			return "invalid-data";
+			return new ResponseEntity<String>("invalid-data", HttpStatus.BAD_REQUEST);
 		}
 	}
 }
